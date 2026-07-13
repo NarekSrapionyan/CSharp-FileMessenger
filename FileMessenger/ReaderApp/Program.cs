78a7305fa733ce6d5e379c0321197f929de4729c
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-07-13 15:55:41 +0400

    Initial solution structure with WriterApp and ReaderApp

---
+++ FileMessenger/ReaderApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace ReaderApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ FileMessenger/WriterApp/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace WriterApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
