77f9b8c15004e1d0aac7ad878dd603ffa7ab0aff
Narek Srapionyan <n.srapionyan05@gmail.com>
2026-07-13 16:19:13 +0400

    Added mode selection

--- FileMessenger/WriterApp/Program.cs
+++ FileMessenger/WriterApp/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // string filePath = "file.txt";
+
+        Console.WriteLine(" Please select mode: ");
+        Console.WriteLine(" 1 - Auto flush ");
+        Console.WriteLine(" 2 - Manual flush (Use /flush command) ");
+        Console.WriteLine(" Enter 1 or 2: ");
+
+        string choice = Console.ReadLine();
+        bool isAutoFlush = choice == "1";
+
+        if (isAutoFlush) Console.WriteLine("Working mode: Auto Flush ");
+        else Console.WriteLine("Working mode: Manual Flush ");
+
     }
 }
